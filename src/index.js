66478bf343764149bd994e0bb5ec9f545e424438
errolgh <ewatson@ggoutfitters.com>
2020-12-19 03:30:47 -0500

    switching to desktop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+
+const store = createStore()
 
 ReactDOM.render(
   <React.StrictMode>
